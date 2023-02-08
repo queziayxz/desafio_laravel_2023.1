40212df8274d97468ff80effd5f563f739c1a763
quezia <quezia.emanuelly2003@gmail.com>
2023-02-08 17:31:42 -0300

    arrumando a dashboard

--- database/migrations/2023_02_07_205412_create_students_table.php
+++ database/migrations/2023_02_07_205412_create_students_table.php
@@ -15,6 +15,14 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->date('birth_date');
+            $table->string('address');
+            $table->string('phone_number');
+            $table->date('register_date');
+            $table->date('pay_day');
+            $table->date('expiration_date');
             $table->timestamps();
         });
     }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,20 @@
 <x-app-layout>
+
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
+    <x-card>
+        <div class="card">
+            <img src="../img/funcionarios.jpg" class="card-img-top" alt="Imagem dos funcioários">
+
+            <div class="card-body">
+                <a href="{{ route('user.index') }}" class="btn btn-primary">Vizualizar Funcioários</a>
+            </div>
             </div>
         </div>
-    </div>
+    </x-card>
+
 </x-app-layout>
