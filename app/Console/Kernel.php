2c54ac2e2e51f77c608cc60090558dc958809622
quezia <quezia.emanuelly2003@gmail.com>
2023-03-02 10:59:31 -0300

    configurando o arquivo kernel e criando o job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,11 +2,17 @@
 
 namespace App\Console;
 
+use DateTimeZone;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+
+    protected $commands = [
+        \App\Console\Commands\AutomaticMailCommand::class,
+    ];
+
     /**
      * Define the application's command schedule.
      *
@@ -15,7 +21,12 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('send:AutomaticMailCommand')->dailyAt('11:00');
+    }
+
+    protected function scheduleTimezone(): DateTimeZone|string|null
+    {
+        return 'America/Sao_Paulo';
     }
 
     /**
@@ -29,4 +40,5 @@ class Kernel extends ConsoleKernel
 
         require base_path('routes/console.php');
     }
+
 }
