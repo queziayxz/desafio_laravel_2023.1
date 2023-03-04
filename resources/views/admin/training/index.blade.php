c47ad3480b39ee4dab7e38c68d1fa56f8d0cdbcd
quezia <quezia.emanuelly2003@gmail.com>
2023-03-04 11:21:01 -0300

    fazendo a responsividade da tela index dos treinos

--- resources/views/admin/training/index.blade.php
+++ resources/views/admin/training/index.blade.php
@@ -12,9 +12,7 @@
             <thead>
                 <tr>
                     <th scope="col">Aluno</th>
-                    <th scope="col">Funcionário</th>
                     <th scope="col">Data</th>
-                    <th scope="col">Hora</th>
                     <th scope="col"></th>
                 </tr>
             </thead>
@@ -22,21 +20,19 @@
                 @foreach($trainings as $training)
                     <tr>
                         <td>{{ $training->student->name }}</td>
-                        <td>{{ $training->user->name }}</td>
                         <td>{{ date("d/m/Y",strtotime(substr($training->start_date, 0, 10))) }}</td>
-                        <td>{{ substr($training->start_date, 11, 5) }}</td>
-                        <td class="d-flex  justify-content-evenly">
-                            <a href="{{ route('training.show',$training->id) }}">
+                        <td class="d-flex flex-wrap justify-content-center">
+                            <a href="{{ route('training.show',$training->id) }}" class="mr-2">
                                 <x-secondary-button class="btn-secondary"><i class="fas fa-search"></i></x-secondary-button>
                             </a>
-                            <a href="{{ route('training.edit',$training->id) }}">
+                            <a href="{{ route('training.edit',$training->id) }}" class="mr-2">
                                 <x-primary-button class="btn-primary"><i class="fas fa-pen"></i></x-primary-button>
                             </a>
                             <x-form action="{{ route('training.destroy', $training->id) }}" method="POST">
                             
                                 @csrf
                                 @method('delete')
-                                <x-danger-button type="submit" class="btn-primary"><i class="fas fa-trash"></i></x-danger-button>
+                                <x-danger-button type="submit" class="btn-primary mr-2"><i class="fas fa-trash"></i></x-danger-button>
 
                         </x-form>
                         </td>
