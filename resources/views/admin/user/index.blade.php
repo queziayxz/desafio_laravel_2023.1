16b767192669338717493ad6c4a6c9a743966801
quezia <quezia.emanuelly2003@gmail.com>
2023-03-04 10:58:24 -0300

    fazendo a responsividade da tela index dos funcionários

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -15,7 +15,6 @@
                 <tr>
                     <th scope="col">Nome</th>
                     <th scope="col">Email</th>
-                    <th scope="col">Turno</th>
                     <th ></th>
                 </tr>
             </thead>
@@ -24,21 +23,20 @@
                     @if($user->type == 1)
                         <tr>
                             <td scope="row">{{ $user->name }}</td>
-                            <td class="mail">{{ $user->email }}</td>
-                            <td>{{ $user->work_time ?? "" }}</td>
-                            <td class="d-flex  justify-content-evenly">
+                            <td class="text-break">{{ $user->email }}</td>
+                            <td class="d-flex flex-wrap justify-content-evenly">
                                 <a href="{{ route('user.show',$user->id) }}">
-                                    <x-secondary-button class="btn-secondary"><i class="fas fa-search"></i></x-secondary-button>
+                                    <x-secondary-button class=" btn btn-secondary link-acoes"><i class="fas fa-search"></i></x-secondary-button>
                                 </a>
                                 <a href="{{ route('user.edit',$user->id) }}">
-                                    <x-primary-button class="btn-primary"><i class="fas fa-pen"></i></x-primary-button>
+                                    <x-primary-button class="btn-primary link-acoes"><i class="fas fa-pen"></i></x-primary-button>
                                 </a>
                                 
                                 @can('delete',$user)
                                     <form action="{{ route('user.destroy', $user->id) }}" method="post">
                                         @csrf
                                         @method('delete')
-                                        <x-danger-button type="submit" class="btn-primary"><i class="fas fa-trash"></i></x-danger-button>
+                                        <x-danger-button type="submit" class="btn-primary link-acoes"><i class="fas fa-trash"></i></x-danger-button>
                                     </form>
                                 @endcan
                             </td>
