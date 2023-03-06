fb84ffd99b7ff38c51b98869ea7fcd7afe851e29
quezia <quezia.emanuelly2003@gmail.com>
2023-03-06 15:44:42 -0300

    alterando o css da view show dos alunos para melhorar a responsividade

--- resources/views/admin/student/show.blade.php
+++ resources/views/admin/student/show.blade.php
@@ -7,14 +7,14 @@
     </x-slot>
 
     <x-card>
-        <p><strong class="fs-5">Nome:</strong> {{ $student->name }}</p>
-        <p><strong class="fs-5">Email:</strong> {{ $student->email }}</p>
-        <p><strong class="fs-5">Data de Nascimento:</strong> {{ date('d/m/Y',strtotime($student->birth_date)) }}</p>
-        <p><strong class="fs-5">Endereço:</strong> {{ $student->address }}</p>
-        <p><strong class="fs-5">Telefone:</strong> {{ $student->phone_number }}</p>
-        <p><strong class="fs-5">Data de Cadastro:</strong> {{ date('d/m/Y',strtotime($student->register_date)) }}</p>
-        <p><strong class="fs-5">Data do Pagamento:</strong> {{ date('d/m/Y',strtotime($student->pay_day)) }}</p>
-        <p><strong class="fs-5">Data de Validade do Contrato:</strong> {{ date('d/m/Y',strtotime($student->expiration_date)) }}</p>
+        <p><strong class="text-show">Nome:</strong> {{ $student->name }}</p>
+        <p><strong class="text-show">Email:</strong> {{ $student->email }}</p>
+        <p><strong class="text-show">Data de Nascimento:</strong> {{ date('d/m/Y',strtotime($student->birth_date)) }}</p>
+        <p><strong class="text-show">Endereço:</strong> {{ $student->address }}</p>
+        <p><strong class="text-show">Telefone:</strong> {{ $student->phone_number }}</p>
+        <p><strong class="text-show">Data de Cadastro:</strong> {{ date('d/m/Y',strtotime($student->register_date)) }}</p>
+        <p><strong class="text-show">Data do Pagamento:</strong> {{ date('d/m/Y',strtotime($student->pay_day)) }}</p>
+        <p><strong class="text-show">Data de Validade do Contrato:</strong> {{ date('d/m/Y',strtotime($student->expiration_date)) }}</p>
     </x-card>
 
 </x-app-layout>
