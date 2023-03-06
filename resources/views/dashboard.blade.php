25d614fcf7c05983507ca88126ecf85a40c0a02b
quezia <quezia.emanuelly2003@gmail.com>
2023-03-06 15:06:13 -0300

    alterando a view do dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,35 +1,20 @@
-<x-app-layout>
-
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
+<x-guest-layout>
+    
+    
     <x-card>
-        <div class="d-flex flex-row justify-content-around">
-            <div class="card" style="width: 18rem;">
-                <img src="/img/funcionarios.jpg" class="card-img-top" alt="Imagem dos funcioários">
-    
-                <div class="card-body d-flex justify-content-center">
-                    <a href="{{ route('user.index') }}" class="btn btn-primary">Vizualizar Funcioários</a>
-                </div>
+        <h1 class="text-center mb-2">DASHBOARD</h1>
+
+        <div class="d-flex flex-row justify-content-around flex-wrap">
+            <div class="card mb-2" style="width: 18rem;">
+                <a href="{{ route('user.index') }}" class="btn btn-primary">Vizualizar Funcioários</a>
+            </div>
+            <div class="card mb-2" style="width: 18rem;">
+                <a href="{{ route('student.index') }}" class="btn btn-primary">Vizualizar Alunos</a>
             </div>
             <div class="card" style="width: 18rem;">
-                <img src="../img/alunos.jpg" class="card-img-top" alt="Imagem dos alunos">
-    
-                <div class="card-body d-flex justify-content-center">
-                    <a href="{{ route('student.index') }}" class="btn btn-primary">Vizualizar Alunos</a>
-                </div>
-            </div>
-            <div class="card" style="width: 18rem;">
-                <img src="../img/treinos.jpg" class="card-img-top" alt="Imagem das aulas">
-    
-                <div class="card-body d-flex justify-content-center">
-                    <a href="{{ route('training.index') }}" class="btn btn-primary">Vizualizar Aulas</a>
-                </div>
+                <a href="{{ route('training.index') }}" class="btn btn-primary">Vizualizar Aulas</a>
             </div>
         </div>
     </x-card>
 
-</x-app-layout>
+</x-guest-layout>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -10,6 +10,9 @@
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
+        <!-- Link do Bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
