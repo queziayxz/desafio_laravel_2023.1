6aa15e6e27e1e15c09391aed0a4109a6254471b1
quezia <quezia.emanuelly2003@gmail.com>
2023-03-08 12:12:36 -0300

    tirando a classe btn no botão de show no intex do user

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -26,7 +26,7 @@
                             <td class="text-break">{{ $user->email }}</td>
                             <td class="d-flex flex-wrap justify-content-evenly">
                                 <a href="{{ route('user.show',$user->id) }}">
-                                    <x-secondary-button class=" btn btn-secondary link-acoes"><i class="fas fa-search"></i></x-secondary-button>
+                                    <x-secondary-button class=" btn-secondary link-acoes"><i class="fas fa-search"></i></x-secondary-button>
                                 </a>
                                 <a href="{{ route('user.edit',$user->id) }}">
                                     <x-primary-button class="btn-primary link-acoes"><i class="fas fa-pen"></i></x-primary-button>
