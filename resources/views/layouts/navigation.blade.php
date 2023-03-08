5c82ef927bddd30fe23720909f626ae92be06711
quezia <quezia.emanuelly2003@gmail.com>
2023-03-08 11:50:39 -0300

    colocando o link de enviar email na navbar responsiva

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -101,6 +101,13 @@
                 {{ __('Treinos') }}
             </x-responsive-nav-link>
         </div>
+        @can('create',auth()->user())
+            <div class="pt-2 pb-3 space-y-1">
+                <x-responsive-nav-link :href="route('email.create')" :active="request()->routeIs('email.create')">
+                    {{ __('Enviar E-mail') }}
+                </x-responsive-nav-link>
+            </div>
+        @endcan
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
