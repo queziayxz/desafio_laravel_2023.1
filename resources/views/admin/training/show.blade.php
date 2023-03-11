3cc144e17fec94a51b208c9941166981174ed046
quezia <quezia.emanuelly2003@gmail.com>
2023-03-11 09:31:25 -0300

    adicionando a class de respondsividade no show do training

--- resources/views/admin/training/show.blade.php
+++ resources/views/admin/training/show.blade.php
@@ -7,12 +7,12 @@
     </x-header>
 
     <x-card>
-        <p><strong>Aluno(a):</strong> {{ $training->student->name }}</p>
-        <p><strong>Funcionário que agendou a aula:</strong> {{ $training->user->name }}</p>
-        <p><strong>Data:</strong> {{ date("d/m/Y",strtotime(substr($training->start_date, 0, 10))) }}</p>
-        <p><strong>Hora Início:</strong> {{ substr($training->start_date, 11, 5) }}</p>
-        <p><strong>Hora Término:</strong> {{ substr($training->end_date, 11, 5) }}</p>
-        <p><strong>Preço:</strong> R${{ $training->cost }}</p>
+        <p><strong class="text-show">Aluno(a):</strong> {{ $training->student->name }}</p>
+        <p><strong class="text-show">Funcionário que agendou a aula:</strong> {{ $training->user->name }}</p>
+        <p><strong class="text-show">Data:</strong> {{ date("d/m/Y",strtotime(substr($training->start_date, 0, 10))) }}</p>
+        <p><strong class="text-show">Hora Início:</strong> {{ substr($training->start_date, 11, 5) }}</p>
+        <p><strong class="text-show">Hora Término:</strong> {{ substr($training->end_date, 11, 5) }}</p>
+        <p><strong class="text-show">Preço:</strong> R${{ $training->cost }}</p>
     </x-card>
 
 </x-app-layout>
